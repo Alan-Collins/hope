d874f41ce2f30d6fe7cdbd7c8058e64816c16238
Alan Collins <alan.collins@bath.edu>
2022-12-29 13:01:21 -0500

    add fn to read fasta to HashMap

--- src/io.rs
+++ src/io.rs
@@ -5,8 +5,10 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
 
 use crate::homopolymer::HomopolymerRecord;
+
 
 
 pub fn read_homo_pol_file(filename: String) -> Vec<HomopolymerRecord> {
@@ -40,4 +42,33 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+pub fn read_fasta(filename: String) -> HashMap<String, String> {
+
+    let mut fasta_map: HashMap<String, String> = HashMap::new();
+    let mut header = String::new();
+    let mut seq = String::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            if let Ok(l) = line {
+                if l.starts_with(">") {
+                    if header.is_empty() {
+                        header.push_str(&l[1..]);
+                        seq.clear();
+                    } else {
+                        fasta_map.insert(header.to_string(), seq.to_string());
+                        header.clear();
+                        header.push_str(&l[1..]);
+                        seq.clear();
+                    }
+                } else {
+                    seq.push_str(l.as_str());
+                }
+            }
+        }
+        fasta_map.insert(header.to_string(), seq.to_string());
+    }
+    fasta_map
 }
